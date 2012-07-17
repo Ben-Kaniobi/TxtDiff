221096e1ef6b81d7fd4fff6cb69245fef252ab4c
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2012-07-17 15:01:43 +0200

    Added project

---
+++ TxtDiff/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frm_TxtDiff
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_TxtDiff));
+            this.spl_Texts = new System.Windows.Forms.SplitContainer();
+            this.txt_A = new System.Windows.Forms.RichTextBox();
+            this.txt_B = new System.Windows.Forms.RichTextBox();
+            this.lbl_A = new System.Windows.Forms.Label();
+            this.lbl_B = new System.Windows.Forms.Label();
+            this.btn_comp = new System.Windows.Forms.CheckBox();
+            this.txt_comp = new System.Windows.Forms.RichTextBox();
+            this.chb_wordwrap = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.spl_Texts)).BeginInit();
+            this.spl_Texts.Panel1.SuspendLayout();
+            this.spl_Texts.Panel2.SuspendLayout();
+            this.spl_Texts.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spl_Texts
+            // 
+            this.spl_Texts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.spl_Texts.IsSplitterFixed = true;
+            this.spl_Texts.Location = new System.Drawing.Point(12, 33);
+            this.spl_Texts.Name = "spl_Texts";
+            // 
+            // spl_Texts.Panel1
+            // 
+            this.spl_Texts.Panel1.Controls.Add(this.txt_A);
+            // 
+            // spl_Texts.Panel2
+            // 
+            this.spl_Texts.Panel2.Controls.Add(this.txt_B);
+            this.spl_Texts.Size = new System.Drawing.Size(400, 157);
+            this.spl_Texts.SplitterDistance = 199;
+            this.spl_Texts.SplitterWidth = 2;
+            this.spl_Texts.TabIndex = 0;
+            // 
+            // txt_A
+            // 
+            this.txt_A.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_A.Location = new System.Drawing.Point(0, 0);
+            this.txt_A.Name = "txt_A";
+            this.txt_A.Size = new System.Drawing.Size(199, 157);
+            this.txt_A.TabIndex = 6;
+            this.txt_A.Text = "";
+            this.txt_A.TextChanged += new System.EventHandler(this.txt_A_TextChanged);
+            this.txt_A.GotFocus += new System.EventHandler(this.txt_A_GotFocus);
+            // 
+            // txt_B
+            // 
+            this.txt_B.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_B.Location = new System.Drawing.Point(0, 0);
+            this.txt_B.Name = "txt_B";
+            this.txt_B.Size = new System.Drawing.Size(199, 157);
+            this.txt_B.TabIndex = 7;
+            this.txt_B.Text = "";
+            this.txt_B.TextChanged += new System.EventHandler(this.txt_B_TextChanged);
+            this.txt_B.GotFocus += new System.EventHandler(this.txt_B_GotFocus);
+            // 
+            // lbl_A
+            // 
+            this.lbl_A.AutoSize = true;
+            this.lbl_A.Location = new System.Drawing.Point(9, 9);
+            this.lbl_A.Name = "lbl_A";
+            this.lbl_A.Size = new System.Drawing.Size(38, 13);
+            this.lbl_A.TabIndex = 1;
+            this.lbl_A.Text = "Text A";
+            // 
+            // lbl_B
+            // 
+            this.lbl_B.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_B.AutoSize = true;
+            this.lbl_B.Location = new System.Drawing.Point(374, 9);
+            this.lbl_B.Name = "lbl_B";
+            this.lbl_B.Size = new System.Drawing.Size(38, 13);
+            this.lbl_B.TabIndex = 1;
+            this.lbl_B.Text = "Text B";
+            // 
+            // btn_comp
+            // 
+            this.btn_comp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn_comp.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btn_comp.Enabled = false;
+            this.btn_comp.Location = new System.Drawing.Point(173, 4);
+            this.btn_comp.Name = "btn_comp";
+            this.btn_comp.Size = new System.Drawing.Size(76, 23);
+            this.btn_comp.TabIndex = 3;
+            this.btn_comp.Text = "Compare";
+            this.btn_comp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_comp.UseVisualStyleBackColor = true;
+            this.btn_comp.CheckedChanged += new System.EventHandler(this.btn_comp_CheckedChanged);
+            this.btn_comp.GotFocus += new System.EventHandler(this.btn_comp_GotFocus);
+            // 
+            // txt_comp
+            // 
+            this.txt_comp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_comp.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_comp.Enabled = false;
+            this.txt_comp.Location = new System.Drawing.Point(12, 33);
+            this.txt_comp.Name = "txt_comp";
+            this.txt_comp.ReadOnly = true;
+            this.txt_comp.Size = new System.Drawing.Size(400, 157);
+            this.txt_comp.TabIndex = 5;
+            this.txt_comp.Text = "";
+            this.txt_comp.Visible = false;
+            this.txt_comp.GotFocus += new System.EventHandler(this.txt_comp_GotFocus);
+            // 
+            // chb_wordwrap
+            // 
+            this.chb_wordwrap.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.chb_wordwrap.AutoSize = true;
+            this.chb_wordwrap.Checked = true;
+            this.chb_wordwrap.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chb_wordwrap.Location = new System.Drawing.Point(175, 196);
+            this.chb_wordwrap.Name = "chb_wordwrap";
+            this.chb_wordwrap.Size = new System.Drawing.Size(78, 17);
+            this.chb_wordwrap.TabIndex = 6;
+            this.chb_wordwrap.Text = "WordWrap";
+            this.chb_wordwrap.UseVisualStyleBackColor = true;
+            this.chb_wordwrap.CheckedChanged += new System.EventHandler(this.chb_wordwrap_CheckedChanged);
+            // 
+            // frm_TxtDiff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 225);
+            this.Controls.Add(this.chb_wordwrap);
+            this.Controls.Add(this.txt_comp);
+            this.Controls.Add(this.btn_comp);
+            this.Controls.Add(this.lbl_B);
+            this.Controls.Add(this.lbl_A);
+            this.Controls.Add(this.spl_Texts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(440, 177);
+            this.Name = "frm_TxtDiff";
+            this.Text = "TxtDiff";
+            this.spl_Texts.Panel1.ResumeLayout(false);
+            this.spl_Texts.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spl_Texts)).EndInit();
+            this.spl_Texts.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer spl_Texts;
+        private System.Windows.Forms.Label lbl_A;
+        private System.Windows.Forms.Label lbl_B;
+        private System.Windows.Forms.CheckBox btn_comp;
+        private System.Windows.Forms.RichTextBox txt_comp;
+        private System.Windows.Forms.RichTextBox txt_A;
+        private System.Windows.Forms.RichTextBox txt_B;
+        private System.Windows.Forms.CheckBox chb_wordwrap;
+
+    }
+}
+
---
+++ TxtDiff/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frm_TxtDiff : Form
+    {
+        public frm_TxtDiff()
+        {
+            InitializeComponent();
+        }
+
+        Color col_del = Color.Red;
+        Color col_add = Color.Green;
+
+        Control FocusedControl;
+
+        private void txt_A_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_A.Text == "" && txt_B.Text == "")
+            {
+                btn_comp.Enabled = false;
+            }
+            else
+            {
+                btn_comp.Enabled = true;
+            }
+        }
+
+        private void txt_B_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_A.Text == "" && txt_B.Text == "")
+            {
+                btn_comp.Enabled = false;
+            }
+            else
+            {
+                btn_comp.Enabled = true;
+            }
+        }
+
+        void AppendColoredText(RichTextBox box, string text, Color color)
+        {
+            //Select text
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = text.Length;
+
+            //Change styles
+            if (color == col_add) box.SelectionFont = new Font(box.SelectionFont, FontStyle.Underline);
+            if (color == col_del) box.SelectionFont = new Font(box.SelectionFont, FontStyle.Strikeout);
+
+            //Change color
+            box.SelectionColor = color;
+
+            box.AppendText(text);
+
+            //Reset styles and color
+            box.SelectionFont = box.Font;
+            box.SelectionColor = box.ForeColor;
+        }
+
+        void compare()
+        {
+            txt_comp.Text = "";
+
+            if (txt_A.Text == txt_B.Text)
+            {
+                AppendColoredText(txt_comp, "The two texts are the same.", Color.Blue);
+                return;
+            }
+
+            string[] linesA = txt_A.Text.Split('\n').Select(s => s.Trim()).ToArray();
+            string[] linesB = txt_B.Text.Split('\n').Select(s => s.Trim()).ToArray();
+
+            int la = 0; //Line number of A
+            int lb = 0; //Line number of B
+            while(la < linesA.Length) //Every line
+            {
+                if (lb >= linesB.Length)
+                {
+                    //txt_comp.Text += "-" + linesA[la];
+                    AppendColoredText(txt_comp, linesA[la], col_del);
+                }
+                else if (linesA[la] == linesB[lb])
+                {
+                    //txt_comp.Text += linesA[la];
+                    AppendColoredText(txt_comp, linesA[la], txt_comp.ForeColor);
+                }
+                else
+                {
+                    //Line added
+                    if ((lb+1 < linesB.Length) && (linesA[la] == linesB[lb+1]))
+                    {
+                        //txt_comp.Text += "+" + linesB[lb] + "\n" + linesA[la];
+                        AppendColoredText(txt_comp, linesB[lb], col_add);
+                        AppendColoredText(txt_comp, "\n" + linesA[la], txt_comp.ForeColor);
+
+                        lb++;
+                    }
+                    //Line deleted
+                    else if ((la + 1 < linesA.Length) && (linesA[la + 1] == linesB[lb]))
+                    {
+                        //txt_comp.Text += "-" + linesA[la] + "\n" + linesB[lb];
+                        AppendColoredText(txt_comp, linesA[la], col_del);
+                        AppendColoredText(txt_comp, "\n" + linesB[lb], txt_comp.ForeColor);
+
+                        la++;
+                    }
+                    //Line changed
+                    else
+                    {
+                        //Begin of word comparsion
+                        //(pretty much the same as with lines)
+
+                        string[] wordsA = linesA[la].Split(' ').Select(s => s.Trim()).ToArray();
+                        string[] wordsB = linesB[lb].Split(' ').Select(s => s.Trim()).ToArray();
+
+                        int wa = 0; //Word number of a
+                        int wb = 0; //Word number of b
+                        while (wa < wordsA.Length) //Every word
+                        {
+                            if (wb >= wordsB.Length)
+                            {
+                                //txt_comp.Text += "-" + wordsA[wa];
+                                AppendColoredText(txt_comp, wordsA[wa], col_del);
+                            }
+                            else if (wordsA[wa] == wordsB[wb])
+                            {
+                                //txt_comp.Text += wordsA[wa];
+                                AppendColoredText(txt_comp, wordsA[wa], txt_comp.ForeColor);
+                            }
+                            else
+                            {
+                                //Word added
+                                if ((wb + 1 < wordsB.Length) && (wordsA[wa] == wordsB[wb + 1]))
+                                {
+                                    //txt_comp.Text += "+" + wordsB[wb] + " " + wordsA[wa];
+                                    AppendColoredText(txt_comp, wordsB[wb], col_add);
+                                    AppendColoredText(txt_comp, " " + wordsA[wa], txt_comp.ForeColor);
+
+                                    wb++;
+                                }
+                                //Word deleted
+                                else if ((wa + 1 < wordsA.Length) && (wordsA[wa + 1] == wordsB[wb]))
+                                {
+                                    //txt_comp.Text += "-" + wordsA[wa] + " " + wordsB[wb];
+                                    AppendColoredText(txt_comp, wordsA[wa], col_del);
+                                    AppendColoredText(txt_comp, " " + wordsB[wb], txt_comp.ForeColor);
+
+                                    wa++;
+                                }
+                                //Word changed
+                                else
+                                {
+                                    //txt_comp.Text += "-" + wordsA[wa] + "+" + wordsB[wb];
+                                    AppendColoredText(txt_comp, wordsA[wa], col_del);
+                                    AppendColoredText(txt_comp, wordsB[wb], col_add);
+                                }
+                            }
+                            if (wa + 1 < wordsA.Length) AppendColoredText(txt_comp, " ", txt_comp.ForeColor);
+
+                            if ((wordsB.Length >= wordsA.Length) && (wa + 1 == wordsA.Length))
+                            {
+                                //Line B is longer -> Append all added words
+                                while (wb + 1 < wordsB.Length)
+                                {
+                                    wb++;
+                                    //txt_comp.Text += " +" + wordsB[wb];
+                                    AppendColoredText(txt_comp, " ", txt_comp.ForeColor);
+                                    AppendColoredText(txt_comp, wordsB[wb], col_add);
+                                }
+                            }
+
+                            wa++;
+                            wb++;
+
+                            //End of word comparison
+                        }
+                    }
+                }
+                if (la + 1 < linesA.Length) AppendColoredText(txt_comp, "\n", txt_comp.ForeColor);
+
+                if ((linesB.Length >= linesA.Length) && (la + 1 == linesA.Length))
+                {
+                    //Text B is longer -> Append all added lines
+                    while (lb + 1 < linesB.Length)
+                    {
+                        lb++;
+                        //txt_comp.Text += "\n+" + linesB[lb];
+                        AppendColoredText(txt_comp, "\n", txt_comp.ForeColor);
+                        AppendColoredText(txt_comp, linesB[lb], col_add);
+                    }
+                }
+
+                la++;
+                lb++;
+            }
+        }
+
+        void comp_checked(bool state)
+        {
+            lbl_A.Visible = !state;
+            lbl_B.Visible = !state;
+
+            txt_A.Enabled = !state;
+            txt_B.Enabled = !state;
+            spl_Texts.Visible = !state;
+
+            txt_comp.Enabled = state;
+            txt_comp.Visible = state;
+        }
+
+        private void btn_comp_CheckedChanged(object sender, EventArgs e)
+        {
+            if (btn_comp.Checked)
+            {
+                //Changes visible
+                comp_checked(true);
+
+                compare();
+            }
+            else
+            {
+                //Original texts visible
+                comp_checked(false);
+            }
+        }
+
+        private void btn_comp_GotFocus(object sender, EventArgs e)
+        {
+            FocusedControl = ActiveControl;
+        }
+
+        private void txt_A_GotFocus(object sender, EventArgs e)
+        {
+            FocusedControl = ActiveControl;
+        }
+
+        private void txt_B_GotFocus(object sender, EventArgs e)
+        {
+            FocusedControl = ActiveControl;
+        }
+
+        private void txt_comp_GotFocus(object sender, EventArgs e)
+        {
+            FocusedControl = ActiveControl;
+        }
+
+        private void chb_wordwrap_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_A.WordWrap = chb_wordwrap.Checked;
+            txt_B.WordWrap = chb_wordwrap.Checked;
+            txt_comp.WordWrap = chb_wordwrap.Checked;
+
+            //Focus back to last focused control
+            FocusedControl.Focus();
+        }
+    }
+}
